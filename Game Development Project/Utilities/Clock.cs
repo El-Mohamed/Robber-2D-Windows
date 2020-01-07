3c8f55a1a8578afb19df3d89719eb17cbb5244ef
Mohamed <elmoufid.mohamed@outlook.com>
2020-01-07 02:37:08 +0100

    Clock Changes

--- Game Development Project/Utilities/Clock.cs
+++ Game Development Project/Utilities/Clock.cs
@@ -12,22 +12,41 @@ namespace Game_Development_Project
     class Clock
     {
         static public int Time { get; set; }
+        static public string TimeText { get; set; } = "";
         static public SpriteFont SpriteFont { get; set; }
         static public Vector2 Position { get; set; }
 
         static public void UpdateTime(GameTime gameTime)
         {
             Time += gameTime.ElapsedGameTime.Milliseconds;
+
+            int minutes = Time / 60000;
+            int seconds = (Time % 60000) / 1000;
+            string strMinutes = Convert.ToString(minutes);
+            string strSeconds = Convert.ToString(seconds);
+
+            if (minutes < 10)
+            {
+                strMinutes = "0" + strMinutes;
+            }
+
+            if (seconds < 10)
+            {
+                strSeconds = "0" + strSeconds;
+            }
+
+            TimeText = strMinutes + ":" + strSeconds;
         }
 
         static public void UpdatePosition(Player player)
         {
-            Position = new Vector2(player.SpriteSheet.Position.X + 10, player.SpriteSheet.Position.Y - (Game1.ScreenHeight / 2) + 100);
+            int OffSet = 15;
+            Position = new Vector2(player.SpriteSheet.Position.X + OffSet, player.SpriteSheet.Position.Y - (Game1.ScreenHeight / 2) + 100);
         }
 
         static public void Draw(SpriteBatch spriteBatch)
-        {
-            spriteBatch.DrawString(SpriteFont, "Time: " + Time / 1000, Position, Color.White);
+        {    
+            spriteBatch.DrawString(SpriteFont, TimeText, Position, Color.White);
         }
 
     }
